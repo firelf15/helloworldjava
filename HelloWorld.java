52df768dd8e4d4bfa1f79b732f7174ceb7d01539
Monika Spielman <monika.spielman@entercom.com>
2016-12-29 17:53:11 -0800

    results

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Pern!"); // Display the string.
+        System.out.println("Hello Coldorn!"); // Display the string.
     }
 }
